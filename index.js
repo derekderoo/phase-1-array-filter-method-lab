9978f84c3df2d712b97399e3573536b03f45e211
Derek DeRoo <derek.l.deroo@gmail.com>
2022-11-29 12:42:09 -0600

    commit tree a7b604573bc4f401615c242db2ccc72fc3045638

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+function findMatching(drivers, person) {
+    function nameMatch(item) {
+      if (item.toUpperCase() === person.toUpperCase()) {
+        return item
+      }
+    }
+   let result = drivers.filter(nameMatch)
+   return result;
+}
+
+function fuzzyMatch(drivers, person) {
+    function nameMatch(item) {
+        if (person === item.substring(0,person.length)) {
+            return item
+        }
+    }
+    let result = drivers.filter(nameMatch)
+    return result;
+}
+
+function matchName(drivers, person) {
+    function nameMatch(item) {
+        if (item.name === person) {
+            return item
+        }
+    }
+    let foundName = drivers.filter(nameMatch)
+        return foundName
+}
